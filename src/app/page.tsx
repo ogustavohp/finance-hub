3777d9f708a39a19dac2fdb5ba877687ca3fb507
Gustavo Henrique <gustavohpereira1.6@gmail.com>
2024-11-07 15:29:36 -0300

    fix: lint error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
-import { UserButton } from "@clerk/nextjs";
-import { auth } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
+import { UserButton } from '@clerk/nextjs'
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
 
 export default async function Home() {
-  const { userId } = await auth();
+  const { userId } = await auth()
   if (!userId) {
-    return redirect("/login");
+    return redirect('/login')
   }
 
   return (
     <div className="flex h-full items-center justify-center">
       <UserButton showName />
     </div>
-  );
+  )
 }
